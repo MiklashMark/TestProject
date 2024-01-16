0aed1e117ee14f66638515cb309345baa12e53c8
Mark Miklash <markmiklash@gmail.com>
2024-01-16 16:44:00 +0300

    Create project

---
+++ user-service/src/test/java/by/bootcamp/markmiklash/test/TestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bootcamp.markmiklash.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
