b74ffbe293c5105707b5d9b7dd2334a3b2818cb6
Mark Miklash <markmiklash@gmail.com>
2024-01-16 20:16:07 +0300

    add ValidationService.

---
+++ user-service/src/main/java/by/bootcamp/markmiklash/userservice/service/validation/ValidationService.java
@@ -0,0 +1,10 @@
+package by.bootcamp.markmiklash.userservice.service.validation;
+
+import by.bootcamp.markmiklash.userservice.core.dto.UserRegistrationDTO;
+
+public class ValidationService implements IValidationService{
+    @Override
+    public void validateRegistration(UserRegistrationDTO user) {
+
+    }
+}
